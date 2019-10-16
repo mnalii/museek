fe93d54ba79b8e771ae10aa88ccf8a4cfda7d2d5
mnali220594 <muhammadnurali303@gmail.com>
2019-10-16 12:31:04 +0700

    [Ali]Edit privacy policy

--- routes/privacyPolicy.js
+++ routes/privacyPolicy.js
@@ -3,7 +3,7 @@ const router = new express.Router()
 
 router.get('/', (req, res) => {
   res.send({
-    content: 'This is privacy policy'
+    content: 'Every personal information that you submit through Museek\'s website, mobile app, or offline activities will be at your own risk. By submitting your personal information to us, you are demmed to have given your consent to the disclosure referred to this policy as required.'
   })
 })
 
