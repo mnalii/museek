c320f70607aa5f96305c50287d9d57299763491d
mnali220594 <muhammadnurali303@gmail.com>
2019-11-21 01:44:25 +0700

    Setup public file

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const userRoutes = require('./routes/user');
-const eventRoutes = require('./routes/event');
-const favoriteRoutes = require('./routes/Favorite');
-const privacyPolicy = require('./routes/privacyPolicy');
+const userRoutes = require("./routes/user");
+const eventRoutes = require("./routes/event");
+const favoriteRoutes = require("./routes/Favorite");
+const privacyPolicy = require("./routes/privacyPolicy");
 // require('dotenv').config()
-const {
-  cloudinaryConfig
-} = require('./config/cloudinaryConfig');
+const { cloudinaryConfig } = require("./config/cloudinaryConfig");
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
 /* istanbul ignore if */
-if (env == 'development' || env == 'test') require('dotenv').config();
+if (env == "development" || env == "test") require("dotenv").config();
 
 const configDb = {
   development: process.env.DBDEV,
@@ -31,30 +29,32 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => console.log('Database connected'))
+  .then(() => console.log("Database connected"))
   .catch(err => console.log(err));
 
-
-app.use('*', cloudinaryConfig);
+app.use("*", cloudinaryConfig);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static("public"));
 /* istanbul ignore if */
-if (env !== 'test') app.use(morgan('tiny'));
+if (env !== "test") app.use(morgan("tiny"));
 
 /* istanbul ignore next */
-app.get("/", (req, res) => {
-  res.send({ hello: "world" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hello: "world" });
+// });
 
 // Router Middleware
-app.use('/api/event', eventRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/favorite', favoriteRoutes);
-app.use('/api/privacy-policy', privacyPolicy);
+app.use("/api/event", eventRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/favorite", favoriteRoutes);
+app.use("/api/privacy-policy", privacyPolicy);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port} and run on ${env}`));
+app.listen(port, () =>
+  console.log(`Listening on port ${port} and run on ${env}`)
+);
 
 module.exports = app;
